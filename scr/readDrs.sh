526e49a29971dd1466af9ac775fd083772f3fc0a
batuhan.osmanoglu@gmail.com <batuhan.osmanoglu@gmail.com@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2009-12-30 22:26:45 +0000

    Added these new functions: scr/readDrs.sh: to read fields from *.drs files scr/modifyRes.sh: to modify values inside *.res files
    Renamed the following:
     scr/setinterfarea.sh scr/getI12sArea.sh
     scr/BoundingBox.sh scr/boundingBox.sh
     scr/readres.sh scr/readRes.sh
     scr/findmaster.sh scr/findMaster.sh
    
    Deleted unnecessary files:
     scr/log scr/temporary.file2 scr/run_doris.sh

---
+++ scr/readDrs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+nargs=$#
+if [ ${nargs} -lt 2 ]
+then 
+cat<<_EOF
+USAGE: readDrs.sh drsFile parameter
+
+INPUT:
+  drsFile: is the DORIS input file (*.drs)
+  parameter: field to be read in the file. 
+
+OUTPUT: 
+  parameter value
+
+LIMITATIONS:
+If the value of the parameter has ":" character the returned field might be wrong.
+
+EXAMPLE:
+	$> ./readDrs.sh /RAID1/batu/adore_MexicoCity2/process/20by20/crops/09828/readfiles.drs M_IN_DAT
+	/data/batu/adore_MexicoCity2/data/09828/dat.040116_ENVISAT1_163630.001_SLC
+	$> ./readDrs.sh /RAID1/batu/adore_MexicoCity2/process/20by20/crops/09828/readfiles.drs LOGFILE
+	/data/batu/adore_MexicoCity2/process/20by20/crops/09828/log.out
+	$> ./readDrs.sh /RAID1/batu/adore_MexicoCity2/process/20by20/crops/09828/crop.drs DUMPBASELINE
+	15 10
+_EOF
+exit 1
+fi
+#Input:
+# readDrs.sh "inputfile" "parameter"
+
+inputfile=${1}
+parameter=${2}
+
+result=`grep ${parameter} ${inputfile} `
+#echo $result
+result=`echo $result | tr -s " "`
+#echo $result
+result=`echo $result | cut -f2- --delimiter=" "`
+#echo $result
+result=${result%%\0}
+#echo $result 
+result2=`echo $result | awk -F" // " '{print $1}' `
+#echo $result2
+#echo RESULT IS:
+if [ -z "$result2" ]
+then
+	echo $result
+else
+	echo $result2
+fi
+
