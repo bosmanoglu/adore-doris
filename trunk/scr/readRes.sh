e28fef62b386b510a6904215a75516d46858d04e
batuhan.osmanoglu <batuhan.osmanoglu@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2010-03-10 13:12:27 +0000

    trunk/scr/readRes.sh   FEAT: Added option to return after column character. If the parameter includes a column(:) character value of the parameter right after the column is returned. This is useful if the value has ":" characters in it. trunk/templates/preparePsiInput.adr   FEAT: This script can be used to generate the input files for TU-DELFT PSI-Toolbox.

--- trunk/scr/readRes.sh
+++ trunk/scr/readRes.sh
@@ -8,7 +8,8 @@ USAGE: readRes.sh resultFile dorisStep parameter
 INPUT:
   resultFile: is the DORIS result file (*.res)
   dorisStep: is the processing step to look for the parameter.
-  parameter: field to be read in the result file. 
+  parameter: field to be read in the result file. If the parameter includes a column(:) character
+   	     value of the parameter is returned. (See last example.)
 
 OUTPUT: 
   parameter value from the result file. 
@@ -23,6 +24,8 @@ EXAMPLE:
     31.238
   $> grep First_pixel_azimuth_time /RAID1/batu/adore_MexicoCity2/process/20by20/crops/09828/09828.res 
     First_pixel_azimuth_time (UTC):                 16-JAN-2004 16:36:31.238
+  $> readRes.sh /RAID1/batu/adore_MexicoCity2/process/20by20/crops/09828/09828.res readfiles "First_pixel_azimuth_time (UTC):"
+    22-Jan-2005 16:45:5.449159
 _EOF
 exit 1
 fi
@@ -35,22 +38,22 @@ parameter=${3}
 notify=${4:-off}
 startline=`grep -n Start_${section} ${inputfile} | cut -f1 -d":"`
 endline=`grep -n End_${section} ${inputfile} | cut -f1 -d":"`
-
 length=`echo ${endline} - ${startline} |bc`
+
 if [[ "${notify}" == "notify" ]]; then
   #get number of hits
-  numHits=`grep -A ${length} Start_${section} ${inputfile} | grep ${parameter}|wc -l`
+  numHits=`grep -A ${length} Start_${section} ${inputfile} | grep "${parameter}"|wc -l`
   if [[ ${numHits} -gt 1 ]]; then
     echo " "
     echo "I found more than 1 match for your selection."    
     echo "Please enter the selection you want me to use."
     for (( c=1; c<=${numHits}; c++ ))
     do
-      matchingLine=`grep -A ${length} Start_${section} ${inputfile} | grep ${parameter}|awk "NR==${c}"`
+      matchingLine=`grep -A ${length} Start_${section} ${inputfile} | grep "${parameter}"|awk "NR==${c}"`
       echo $c : ${matchingLine}      
     done
     read -p "Please enter number between 1 and $((${c} -1)): " -e line2Read
-    result=`grep -A ${length} Start_${section} ${inputfile} | grep ${parameter}|awk "NR==${line2Read}"`    
+    result=`grep -A ${length} Start_${section} ${inputfile} | grep "${parameter}"|awk "NR==${line2Read}"`    
     if [ -z "${result}" ]; then
       echo "Something went wrong."
       echo "Exiting..."
@@ -58,9 +61,14 @@ if [[ "${notify}" == "notify" ]]; then
     fi
   fi
 else
-  result=`grep -A ${length} Start_${section} ${inputfile} | grep ${parameter}`
+  result=`grep -A ${length} Start_${section} ${inputfile} | grep "${parameter}"`
 fi
-result=${result##*:} 	#Get the part after the LAST column 
+
+if [[ ${parameter} == *:* ]]; then
+  result=${result//${parameter}};
+else
+  result=${result##*:} 	#Get the part after the LAST column 
+fi
 result=${result%%//*}   #delete everything after //
 
 #echo "$startline $endline $length $parameter_length"
