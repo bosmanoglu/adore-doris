82265d3292bae1808acb517fa768b209bd3c7a40
batuhan.osmanoglu@gmail.com <batuhan.osmanoglu@gmail.com@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2011-03-13 18:45:54 +0000

    gui/adore-gui.py, gui/adore-gui.ui   Continuing development on the gui. Now can read variables from bash command line to ADORE. scr/adore, scr/.adore.rc   Added Gui mode. Required for reading ADORE variables from Python scr/fun/settings   Feat: Changed the listing to comply with python config files. Prints [section-headers]. set/default.set   Added section headers for gui.

--- trunk/gui/adore-gui.py
+++ trunk/gui/adore-gui.py
@@ -10,6 +10,7 @@ import os
 import sys
 import subprocess
 import shlex
+import ConfigParser
 
 try:
   import vte
@@ -19,10 +20,13 @@ except:
 
 class MyGUI:
 
-  ui_file = open('adore-gui.ui', 'r')
-  ui = ui_file.read()
+  #ui_file = open('adore-gui.ui', 'r')
+  #ui = ui_file.read()
 
-  def __init__( self, title):
+  def __init__( self, title,argv):
+    self.argv=argv or "adore -i"
+    self.set = ConfigParser.ConfigParser()
+    self.setFile="/tmp/adore.set"
     self.window = gtk.Window()
     self.title = title
     self.window.set_title( title)
@@ -40,21 +44,46 @@ class MyGUI:
     self.actiongroup.add_actions(
       [('About', None, '_About'),
        ('File', None, '_File'),
-       ('Quit', None, '_Quit', None, 'Quit ADORE.', self.file_quit),
+       ('Quit', None, '_Quit', '<ALT>q', 'Quit ADORE.', self.file_quit),
        ('Edit', None, '_Edit'),
-       ('Copy', None, '_Copy', None, 'Copy selected text.', self.edit_copy),
-       ('Paste', None, '_Paste', None, 'Paste text from clipboard.', self.edit_paste),
+       ('Copy', None, '_Copy', '<CTRL>c', 'Copy selected text.', self.edit_copy),
+       ('Paste', None, '_Paste','<CTRL>v', 'Paste text from clipboard.', self.edit_paste),
        ('Process', None, '_Process'),
-       ('m_readfiles', None, 'm_readfiles', None, '', self.runMenuName),
-       ('m_porbits', None, 'm_porbits', None, '', self.runMenuName),
-       ('m_crop', None, '_m_crop', None, '', self.runMenuName),
-       ('m_simamp', None, '_m_simamp', None, '', self.runMenuName),
-       ('m_timing', None, '_m_timing', None, '', self.runMenuName),
-       ('m_ovs', None, '_m_ovs', None, '', self.runMenuName),
-       ('s_readfiles', None, 's_readfiles', None, '', self.runMenuName),
-       ('s_porbits', None, 's_porbits', None, '', self.runMenuName),
-       ('s_crop', None, '_s_crop', None, '', self.runMenuName),
-       ('s_ovs', None, '_s_ovs', None, '', self.runMenuName),
+       ('Master', None, '_Master'),
+       ('m_readfiles', None, 'm__readfiles', None, '', self.runMenuName),
+       ('m_porbits', None, 'm__porbits', None, '', self.runMenuName),
+       ('m_crop', None, 'm__crop', None, '', self.runMenuName),
+       ('m_simamp', None, 'm__simamp', None, '', self.runMenuName),
+       ('m_timing', None, 'm__timing', None, '', self.runMenuName),
+       ('m_ovs', None, 'm__ovs', None, '', self.runMenuName),
+       ('m_filtazi', None, 'm__filtazi', None, 'Run after coarsecorr', self.runMenuName),
+       ('Slave', None, '_Slave'),
+       ('s_readfiles', None, 's__readfiles', None, '', self.runMenuName),
+       ('s_porbits', None, 's__porbits', None, '', self.runMenuName),
+       ('s_crop', None, 's__crop', None, '', self.runMenuName),
+       ('s_ovs', None, 's__ovs', None, '', self.runMenuName),
+       ('s_filtazi', None, 's__filtazi', None, 'Run after coarsecorr', self.runMenuName),
+       ('Interferogram', None, '_Interferogram'),
+       ('coarseorb', None, 'coarseorb', None, '', self.runMenuName),
+       ('coarsecorr', None, 'coarsecorr', None, '', self.runMenuName),
+       ('fine', None, 'fine', None, '', self.runMenuName),
+       ('reltiming', None, 'reltiming', None, '', self.runMenuName),
+       ('demassist', None, 'demassist', None, '', self.runMenuName),
+       ('coregpm', None, 'coregpm', None, '', self.runMenuName),
+       ('resample', None, 'resample', None, '', self.runMenuName),
+       ('filtrange', None, 'filtrange', None, '', self.runMenuName),
+       ('interfero', None, 'interfero', None, '', self.runMenuName),
+       ('comprefpha', None, 'comprefpha', None, '', self.runMenuName),
+       ('subtrrefpha', None, 'subtrrefpha', None, '', self.runMenuName),
+       ('comprefdem', None, 'comprefdem', None, '', self.runMenuName),
+       ('subtrrefdem', None, 'subtrrefdem', None, '', self.runMenuName),
+       ('coherence', None, 'coherence', None, '', self.runMenuName),
+       ('filtphase', None, 'filtphase', None, '', self.runMenuName),
+       ('unwrap', None, 'unwrap', None, '', self.runMenuName),
+       ('dinsar', None, 'dinsar', None, '', self.runMenuName),
+       ('slant2htrick', None, 'slant2htrick', None, '', self.runMenuName),
+       ('slant2h', None, 'slant2h', None, '', self.runMenuName),
+       ('geocode', None, 'geocode', None, '', self.runMenuName),
        #('', None, '_', None, '', self.process_),
        ('Settings', None, '_Settings'),
        ('Check', None, '_Check', None, 'Check settings against default values.', self.settings_check),
@@ -63,7 +92,6 @@ class MyGUI:
        ('Save', None, '_Save', None, 'Save settings to current folder.', self.settings_save),
        ('Reset',  None, '_Reset',  None, 'Reset settings to defaults', self.settings_reset),
        ('ShowAbout', gtk.STOCK_ABOUT, '_About', None, 'Show about information', self.show_about),
-       ('Version', gtk.STOCK_INDEX, '_Version', None, 'Show version', self.show_version),
        ])
     # the uimanager
     self.uimanager = gtk.UIManager()
@@ -71,7 +99,8 @@ class MyGUI:
     self.window.add_accel_group( accelgroup)
 
     self.uimanager.insert_action_group( self.actiongroup, 0)
-    self.uimanager.add_ui_from_string(self.ui)
+    #self.uimanager.add_ui_from_string(self.ui)
+    self.uimanager.add_ui_from_file('adore-gui.ui')
     # menu bar
     self.menu_bar = self.uimanager.get_widget( "/MenuBar")
     self.mainbox.pack_start( self.menu_bar, expand=False, fill=False)
@@ -85,14 +114,21 @@ class MyGUI:
     # vte term
     self.v = vte.Terminal()
     self.v.connect("child-exited", lambda term: gtk.main_quit())
-    self.v.fork_command("sh", ["/bin/bash", "-c", "adore -i"])
+    #self.argv="adore -i -u /home/bosmanoglu/tmp/test/settings.set"
+    #self.v.fork_command("sh", ["/bin/bash", "-c", "adore -i" ])
+    #self.v.fork_command("sh", ["/bin/bash", "-c", "adore -i -u /home/bosmanoglu/tmp/test/settings.set"])
+    self.v.fork_command("sh", ["/bin/bash", "-c", self.argv])
+    #self.v.fork_command("sh", shlex.split(self.argv))
     self.mainbox.pack_start(self.v)
     
     # show the box
     self.mainbox.show()
 
-  def main( self):
+  def main(self):
     gtk.main()
+
+  def readSet(self): #readSettings
+    self.set.read(self.setFile)
 
   def runcmd(self, cmd):
     self.v.feed_child(cmd+'\n');
@@ -126,12 +162,27 @@ class MyGUI:
     self.runcmd('settings reset');
 
   def show_about( self, w):
-    os.system("echo ADORE-GUI")#self.answer_label.set_label( "Created by Beda Kosata")
-
-  def show_version( self, w):
-    os.system("echo alpha 0.0.0") #self.answer_label.set_label( "2.1")
+    #os.system("echo ADORE-GUI")#self.answer_label.set_label( "Created by Beda Kosata")
+    about = gtk.AboutDialog()
+    about.set_program_name("ADORE-GUI")
+    about.set_version("0.0.0")
+    about.set_copyright("(c) Batuhan Osmanoglu")
+    about.set_license("GNU_GPL v2")
+    about.set_comments("Automated Doris Environment")
+    about.set_website("http://code.google.com/p/adore-doris/")
+    self.readSet();
+    about.set_logo(gtk.gdk.pixbuf_new_from_file(self.set.get('adore','ADOREFOLDER').strip('\'"') +"/man/adoreDoris_tud_um.png"))
+    about.run()
+    about.destroy()
 
 if __name__ == "__main__":
-  m = MyGUI( "ADORE GUI - alpha 0.0.0")  
+  ADOREGUIFOLDER=os.path.split(os.path.realpath(sys.argv[0]))[0]
+  ADOREFOLDER=os.path.split(ADOREGUIFOLDER)[0]
+  print ADOREFOLDER
+  if len(sys.argv[1:]) >0:
+    argv=ADOREFOLDER + "/scr/adore -g " + " ".join(sys.argv[1:]);
+    print argv
+  m = MyGUI("ADORE GUI",argv)  
   m.main()
+
   
