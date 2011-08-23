1254fbe963fd552f00e4fc7ea0b7843d46acf859
batuhan.osmanoglu@gmail.com <batuhan.osmanoglu@gmail.com@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2011-08-23 16:51:50 +0000

    Added a few more manuals. Started working on generation of HTML version from TXT files.

---
+++ man/bin/replaceText.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Replace Text
+# replacetext.sh textfile filterfile
+#
+# textfile: The file where the text will be read used. 
+# filterfile: The file where several filters are listed 
+#   for search and replace. 
+#
+txtf=${1}
+fltf=${2}
+
+OIFS=${IFS}
+IFS=$'\n'
+for pair in `cat ${fltf}`
+do
+  search=${pair%,*}
+  replace=${pair##*,}
+  #echo search:$search replace:${replace}
+  sed -i "s@${search}@${replace}@g" ${txtf}
+done
+IFS=${OIFS}
