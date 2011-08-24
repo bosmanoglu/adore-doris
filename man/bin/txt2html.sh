35a4c807b9496307e34ffa977ef35e153e331734
batuhan.osmanoglu@gmail.com <batuhan.osmanoglu@gmail.com@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2011-08-24 02:38:00 +0000

    Modified some text files and adding the html files.

--- man/bin/txt2html.sh
+++ man/bin/txt2html.sh
@@ -4,10 +4,11 @@ txtf=${1}
 htmlf=${2}
 
 rm ${htmlf}/links.html
-for file in `ls ${1}`;
+for file in `ls ${1} | grep -v "~"`;
 do 
  echo "<a href=${file}.html>${file}</a><br>" >> ${htmlf}/links.html 
- cp ${txtf}/${file} ${htmlf}/${file}.html
+ echo "<a href=links.html>Back to List</a>" > ${htmlf}/${file}.html
+ sed '/^[[:space:]]*$/d' ${txtf}/${file} >> ${htmlf}/${file}.html
  ./replaceText.sh ${htmlf}/${file}.html txt2html.filter
- echo "<a href=links.html>Functions</a>" >> ${htmlf}/${file}.html
+ echo "<BR><a href=links.html>Back to List</a>" >> ${htmlf}/${file}.html
 done
