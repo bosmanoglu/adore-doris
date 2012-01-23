97e843364ada5b6e75be8ad13a0e1c37e04979f4
batuhan.osmanoglu@gmail.com <batuhan.osmanoglu@gmail.com@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2012-01-23 03:42:50 +0000

    build/build.sh   Minor changes to deal with links. gui/agooey.ui, gui/agooey   Added offline help, right-click menu.

--- build/build.sh
+++ build/build.sh
@@ -5,9 +5,14 @@ rm -rf /tmp/adore/
 svn export ${SVNFOLDER} /tmp/adore/
 # rm unwanted directories
 rm -rf /tmp/adore/build
-rm -rf /tmp/adore/gui
+#rm -rf /tmp/adore/gui
+#Remove links
+#rm -rf /tmp/adore/scr/agooey
+#rm -rf /tmp/adore/scr/cpxview
+#rm -rf /tmp/adore/drs/4.02
+#rm -rf /tmp/adore/drs/4.06
 #get version
-version=${1}
+version=`cat ${SVNFOLDER}/version`
 cat > adore.list <<_EOF
 \$prefix=/opt/adore
 \$exec_prefix=\${prefix}
@@ -16,6 +21,7 @@ cat > adore.list <<_EOF
 %product ADORE - Automated DORIS Environment
 %copyright 2009-2011, Batuhan Osmanoglu.
 %vendor Batuhan Osmanoglu
+%maintainer Batuhan Osmanoglu <batuhan.osmanoglu@gmail.com>
 %license LICENSE
 %readme README
 %description ADORE is a shell environment designed to simplify and streamline interferometry with DORIS.
@@ -26,8 +32,17 @@ cat > adore.list <<_EOF
 %requires gdal-bin
 
 _EOF
-mkepmlist -g root -u root --prefix /opt/adore /tmp/adore/ >> adore.list
+#Add adore link to system bin directory.
 echo "l 000 root root /usr/bin/adore /opt/adore/scr/adore" >> adore.list
+#Add links
+#echo "l 000 root root /opt/adore/scr/agooey /opt/adore/gui/agooey" >> adore.list
+#echo "l 000 root root /opt/adore/scr/cpxview /opt/adore/lib/python/cpxview.py" >> adore.list
+#echo "l 000 root root /usr/bin/agooey /opt/adore/scr/agooey" >> adore.list
+#echo "l 000 root root /opt/adore/drs/4.02 /opt/adore/drs/4.03" >> adore.list
+#echo "l 000 root root /opt/adore/drs/4.06 /opt/adore/drs/4.05" >> adore.list
+#create list
+mkepmlist -g root -u root --prefix /opt/adore /tmp/adore/ >> adore.list
+#Done replace links
 cd ${SVNFOLDER}/build
 sudo epm -a all -f deb adore
-sudo epm -a all -f rpm adore
+#sudo epm -a all -f rpm adore
