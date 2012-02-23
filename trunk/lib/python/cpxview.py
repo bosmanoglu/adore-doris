trunk/lib/python/deramp.py   FEAT: Added weighting and made the multilooking a parameter. trunk/lib/python/adore.py   BUGFIX: Added geocoding step for res2dict.   BUGFIX: Corrected the calculation of number of parameters for coregpm polygons   FEAT: Added parseSettings('settings.set') to read the ADORE settings into     python. However this has to be the settings file created with "settings list",     not the regular adore settings trunk/lib/python/cpxview.py   FEAT: Now uses memmap to reduce the memory footprint of large files. Especially     useful when displaying large files with -M20/20 etc. trunk/scr/fun/scenes   FEAT: Started working on "scenes add" which will add scenes to the existing data     folder. NOT COMPLETE.. CURRENTLY A BUG. trunk/scr/fun/openInIpython   FEAT: Now supports three modes. Without any parameters just loads the ipython.rc     With a file as parameter, runs that file in ipython after ipython.rc     With a command as parameter, runs that command in ipython after ipython.rc trunk/scr/fun/dem   FEAT: Added support for ESRI .hdr labelled *.BIL format trunk/scr/fun/functions   BUGFIX: Changed the calculate_coh_winsize so that for single look interferograms   the coh_window is always 5 5 or bigger. trunk/scr/fun/report   FEAT: Now displays the results in a table format. Also displays the location of     html file at the end. trunk/scr/process/resample   BUGFIX: the autoFlag was not working properly. Now it works as expected. i.e.     If the user does not set rs_dbow or rs_dbow_geo, at the end of resample     these settings go back to "0 0 0 0".

trunk/lib/python/cpxview.py
@@ -218,13 +218,15 @@ def getdata(fname, width, dataFormat, length=0, byteSwapFlag=False):
         complexFlag=True;
     ### Handle the short format specifier: i.e. cr4    
     if dataFormat=="cr4":
-        datatype="f4"        
-        complexFlag=True;
+        #datatype="f4"
+        datatype='complex64'
+        complexFlag=False;
     elif dataFormat=="r4":
         datatype="f4"
     if dataFormat=="cr8":
-        datatype="f8"        
-        complexFlag=True;
+        #datatype="f8"
+        datatype='complex128'
+        complexFlag=False;
     elif dataFormat=="r8":
         datatype="f8"
     elif dataFormat=="ci2":
@@ -259,13 +261,17 @@ def getdata(fname, width, dataFormat, length=0, byteSwapFlag=False):
         length=int(length);
 
 #    if complexFlag:
-#        datatype=np.dtype( [ ("real", datatype), ("imag", datatype) ] );
+#                 np.dtype((np.int32,{'real':(np.int16, 0),'imag':(np.int16, 2)})
+#        datatype=np.dtype( ( {"real":(datatype,0), ("imag", datatype) ] );
     data=np.memmap(fname, dtype=datatype,mode="r", shape=(length,width))
     #data=np.fromfile(fname, datatype ,width*length).reshape(length, width)
     if byteSwapFlag:
         #data=data.byteswap()
-        data.byteswap(True)
-    if complexFlag:
+        try:
+            data.byteswap(True)
+        except:
+            data=data.byteswap(); #memmap can not be byteswapped in-place.
+    if complexFlag: 
         data=data[:,0:-1:2]+1j*data[:,1::2]
 #    else:
 #        data=np.fromfile(fname, datatype ,width*length).reshape(length, width)

trunk/lib/python/deramp.py
@@ -38,22 +38,26 @@ scipy.pkgload('optimize')
 def usage():
     print __doc__
 
-def deramp(inData, estData=None):
-    X,Y = np.meshgrid(np.r_[0:inData.shape[1]],np.r_[0:inData.shape[0]])
+def deramp(inData, estData=None, multilook=[1000,1000], weight=None):
+    X,Y = np.meshgrid(np.r_[0:inData.shape[0]],np.r_[0:inData.shape[1]])
     #X=X[inData<0.1];
-    fitX=X.ravel()[0::1000]
+    fitX=X.ravel()[0::multilook[0]]
     #Y=Y[inData<0.1];
-    fitY=Y.ravel()[0::1000]
+    fitY=Y.ravel()[0::multilook[1]]
     if estData is not None:
         inData=inData-estData;
-    fitData=inData.ravel()[0::1000]
+    fitData=inData[fitX,fitY]#.ravel()[0::multilook]
+    if weight is not None:
+        w=weight[fitX,fitY]
+    else:
+        w=np.ones(fitX.shape)
     
     fitfunc = lambda p, x, y: p[0]+p[1]*x+p[2]*y 
-    errfunc = lambda p, x, y, z: fitfunc(p,x,y) - z
-    planefit, success=scipy.optimize.leastsq(errfunc, fitData, args=(fitX,fitY,fitData))
+    errfunc = lambda p, x, y, z, w: w*(fitfunc(p,x,y) - z)
+    planefit, success=scipy.optimize.leastsq(errfunc, fitData, args=(fitX,fitY,fitData,w))
     
     #X,Y = np.meshgrid(np.r_[0:inData.shape[0]],np.r_[0:inData.shape[1]])
-    outData=inData-fitfunc(planefit,X,Y)
+    outData=inData-fitfunc(planefit,X.T,Y.T)
     if estData is not None:
         outData=outData+estData
     return outData