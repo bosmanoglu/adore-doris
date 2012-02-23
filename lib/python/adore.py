trunk/lib/python/deramp.py   FEAT: Added weighting and made the multilooking a parameter. trunk/lib/python/adore.py   BUGFIX: Added geocoding step for res2dict.   BUGFIX: Corrected the calculation of number of parameters for coregpm polygons   FEAT: Added parseSettings('settings.set') to read the ADORE settings into     python. However this has to be the settings file created with "settings list",     not the regular adore settings trunk/lib/python/cpxview.py   FEAT: Now uses memmap to reduce the memory footprint of large files. Especially     useful when displaying large files with -M20/20 etc. trunk/scr/fun/scenes   FEAT: Started working on "scenes add" which will add scenes to the existing data     folder. NOT COMPLETE.. CURRENTLY A BUG. trunk/scr/fun/openInIpython   FEAT: Now supports three modes. Without any parameters just loads the ipython.rc     With a file as parameter, runs that file in ipython after ipython.rc     With a command as parameter, runs that command in ipython after ipython.rc trunk/scr/fun/dem   FEAT: Added support for ESRI .hdr labelled *.BIL format trunk/scr/fun/functions   BUGFIX: Changed the calculate_coh_winsize so that for single look interferograms   the coh_window is always 5 5 or bigger. trunk/scr/fun/report   FEAT: Now displays the results in a table format. Also displays the location of     html file at the end. trunk/scr/process/resample   BUGFIX: the autoFlag was not working properly. Now it works as expected. i.e.     If the user does not set rs_dbow or rs_dbow_geo, at the end of resample     these settings go back to "0 0 0 0".

lib/python/adore.py
@@ -347,6 +347,19 @@ def process2dict(fileDict, processName):
                   'Number of pixels': None,   
                   'Program used for unwrapping': None,
                   }                  
+    elif processName == 'geocoding':
+        reDict = {'Data_output_file_hei': "Data_output_file (slant2h):[\s]+(.*)",
+                  'Data_output_file_phi': None,
+                  'Data_output_file_lamda': None,
+                  'Data_output_format': "Data_output_format:[\s]+(.*)",
+                  'First_line': None,
+                  'Last_line': None,
+                  'First_pixel': None,
+                  'Last_pixel': None,   
+                  'Multilookfactor_azimuth_direction': None,
+                  'Multilookfactor_range_direction': None,
+                  }                        
+    
 #    elif processName == '':
 #        reDict = {'': None,
 #                  '': None,
@@ -377,10 +390,11 @@ def process2dict(fileDict, processName):
             out['results']=csv2Array(fileDict, lStart+10, int(out['Number_of_correlation_windows']), 6, dtype=np.float)
         except:
             pass
-    if processName=="comp_coregpm" and out['Degree_cpm']:        
-        numCoef=[1,3,6,10]#2*(int(out['Degree_cpm'])+1)
-        out['Estimated_coefficientsL']=csv2Array(fileDict,lStart+4, numCoef[int(out['Degree_cpm'])], 3, dtype=np.float)
-        out['Estimated_coefficientsP']=csv2Array(fileDict,lStart+6+numCoef[int(out['Degree_cpm'])], numCoef[int(out['Degree_cpm'])], 3, dtype=np.float)        
+    if processName=="comp_coregpm" and out['Degree_cpm']:       
+        #Below equation is from doris/src/utilities.hh Line:100, 
+        numCoef=int(np.fix(0.5*((out['Degree_cpm']+1)**2+out['Degree_cpm']+1)))#[1,3,6,10]#2*(int(out['Degree_cpm'])+1)
+        out['Estimated_coefficientsL']=csv2Array(fileDict,lStart+4, numCoef, 3, dtype=np.float) # numCoef[int(out['Degree_cpm'])]
+        out['Estimated_coefficientsP']=csv2Array(fileDict,lStart+6+numCoef  , numCoef, 3, dtype=np.float)        
     return out
     
 def getval(fileDict, key, lines=None, processName=None, regexp=None):
@@ -558,4 +572,17 @@ def getProduct(rdict, process, filename=None, width=None, dataFormat=None):
     if dataFormat is None:
         dataFormat=rdict[process]['Data_output_format']        
     return getdata(filename, width, dataFormat);
-       
+
+def parseSettings(filename):
+    """
+    s=parseSettings('settings.set')
+    """
+    import ConfigParser
+    class AdoreConfigParser(ConfigParser.RawConfigParser):
+        def get(self, section, option):
+            val = ConfigParser.RawConfigParser.get(self, section, option)
+            return val.lstrip('"').rstrip('"')
+
+    s=AdoreConfigParser();
+    s.read(filename);
+    return s