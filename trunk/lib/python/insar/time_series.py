b803a2f21b52626676a8ce827b310725f2119f96
batuhan.osmanoglu@gmail.com <batuhan.osmanoglu@gmail.com@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2012-12-27 22:48:04 +0000

    trunk/scr/fun/giant trunk/lib/python/giant/prepxml_MINTS.py   FEAT: Added support for mints. not well tested. trunk/lib/python/insar/time_series.py   FEAT: A new function for plotting bperp/btemp plot from interferograms.

--- trunk/lib/python/insar/time_series.py
+++ trunk/lib/python/insar/time_series.py
@@ -27,14 +27,14 @@ def select_pairs(scene, btemp, bperp, bdopp=None, btemp_limits=[0,400],bperp_lim
                 continue
             if bdopp is not None:
 		# if bdopp is out of range skip
-                if ( abs(bdopp[m]-bdopp[s])> bdopp_limits[1] ) | ( abs(bdopp[m]-bdopp[s]) < btemp_limits[0] ):
+                if ( abs(bdopp[m]-bdopp[s])> bdopp_limits[1] ) | ( abs(bdopp[m]-bdopp[s]) < bdopp_limits[0] ):
                     continue
             pairs.append((scene[m], scene[s]));
     return pairs
 ###############################################################
 ###############################################################
 ###############################################################
-def select_pairs_from_baselines(baselines_file,btemp_limits=[0,400],bperp_limits=[0,300],bdopp_limits=None):
+def select_pairs_from_baselines(baselines_file,btemp_limits=[0,400],bperp_limits=[0,300],bdopp_limits=[0,500]):
     """select_pairs_from_baselines(baselines_file)
     """
     baseline_dtype=N.dtype([
@@ -74,13 +74,61 @@ def bperp_btemp(drsFiles):
     return bperp, btemp        
 ###############################################################
 ###############################################################
+###############################################################        
+def bperp_btemp_master_slave(drsFiles):
+    from dateutil import parser
+    
+    mresfiles=[]
+    sresfiles=[]
+    iresfiles=[]
+    for f in drsFiles:
+        d=adore.drs2dict(f);
+        mresfiles.append(d['general']['m_resfile'].strip());
+        sresfiles.append(d['general']['s_resfile'].strip());
+        iresfiles.append(d['general']['i_resfile'].strip());
+    
+    bperp=[]
+    btemp=[]
+    master=[]
+    slave=[]
+    for f in N.r_[0:len(mresfiles)]:
+        mobj=adore.dict2obj(adore.res2dict(mresfiles[f]))
+        sobj=adore.dict2obj(adore.res2dict(sresfiles[f]))
+        ires=adore.res2dict(iresfiles[f])
+        iobj=adore.dict2obj(ires)
+        master.append( parser.parse(mobj.readfiles.First_pixel_azimuth_time) )
+        slave.append( parser.parse(sobj.readfiles.First_pixel_azimuth_time) )
+        bperp.append( iobj.coarse_orbits.Bperp )
+        btemp.append( iobj.coarse_orbits.Btemp )
+    return bperp, btemp, master, slave        
 ###############################################################
-#def plot_bperp_btemp(drsFiles):
-#    fg=P.figure()
-#    bperp,btemp=bperp_btemp(drsFiles)
-#    scatter(btemp, bperp)
-#    
-#    return fg
+###############################################################
+###############################################################
+def plot_bperp_btemp(drsFiles):
+    fg=P.figure()
+    bperp,btemp,master,slave=bperp_btemp_master_slave(drsFiles)
+    
+    K=len(bperp)
+    allDates=master + slave #merge master and slave lists
+    allDates=N.sort(list(set(allDates)));
+    I=len(allDates); #Number of images (dates)
+    temporalSampling=[x.days for x in N.diff(N.sort(list(set(allDates)))).ravel() ]
+    A=N.zeros([K+1,I]);
+    tS=temporalSampling
+    for k in xrange(K):
+        A[k,:]=(allDates==master[k])*1+(allDates==slave[k])*-1; 
+    A[-1,0]=1
+    t=N.dot(N.linalg.pinv(A),btemp+[0]) #merge zero to the end of list
+    p=N.dot(N.linalg.pinv(A),bperp+[0]) #merge zero to the end of list
+    
+    P.scatter(t,p)
+    
+    for k in xrange(K):
+        tk=t[allDates==master[k]]
+        pk=p[allDates==master[k]]
+        P.plot([tk, tk-btemp[k]], [pk, pk-bperp[k]] )
+    
+    return fg
 ###############################################################
 ###############################################################
 ###############################################################        
