79eaf7e13c4c82357afa9a2ef9e1b3e2ad9aa22d
batuhan.osmanoglu@gmail.com <batuhan.osmanoglu@gmail.com@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2013-02-02 02:59:53 +0000

    trunk/lib/python/insar/time_series.py   BUGFIX: Fixed the upside down baselines plot (from i_resfiles)   FEAT: Added a new function (plot_bperp_btemp) to plot network from baselines file and pairs file. trunk/lib/python/basic/__init__.py   Removed gridSearch function. Has been problematic?? trunk/lib/python/giant.py   BUGFIX: Fixed the wrong path issue for GIANT results.   FEAT: Added ensemble_coherence function. Might have issues... trunk/scr/fun/s_fbd2fbs, trunk/scr/fun/m_fbd2fbs   FEAT: Support for ALOS fbd fbs interferometry trunk/scr/fun/previousSteps   FEAT: Scan the resultfiles and display executed steps. trunk/scr/fun/report   BUGFIX: Removed the unnecessary echo of coh_multilook trunk/scr/fun/i12s   FEAT: Added clear function to remove defined pairs. trunk/scr/fun/tabcompletion   BUGFIX: The _i12s function definition was missing   FEAT: added additional keywords for giant trunk/scr/fun/superMasterResample   FEAT: Support for resampling all crops to a super master without the scripts trunk/scr/fun/functions   FEAT: added definitions for superMasterResample, s_fbd2fbs and m_fbd2fbs trunk/scr/process/coarseorb   Tested the possibility of automatically resampling fbd2fbs. But this is problematic. Now we have separate commands. trunk/scr/process/coarsecorr   BUGFIX: Now we have much better support for manual coregistration with master and slave having different widths/formats etc. trunk/scr/rs2pn   FEAT: Added some keywords for format conversion (eg. complex_real4 ==> cr4)

--- lib/python/giant.py
+++ lib/python/giant.py
@@ -33,10 +33,48 @@ class Object:
             except:
                 self.io.pparsxml=None
                 self.ppars=None
-            if os.path.exists(os.path.join(self.dpars.data.dirs.h5dir, 'RAW-STACK.h5')):
-                self.RAW_STACK=ts.loadh5(os.path.join(self.dpars.data.dirs.h5dir, 'RAW-STACK.h5'))
-            if os.path.exists(os.path.join(self.dpars.data.dirs.h5dir, 'PROC-STACK.h5')):
-                self.PROC_STACK=ts.loadh5(os.path.join(self.dpars.data.dirs.h5dir, 'PROC-STACK.h5'))
-            if os.path.exists(os.path.join(self.dpars.data.dirs.h5dir, 'LS-PARAMS.h5')):
-                self.LS_PARAMS=ts.loadh5(os.path.join(self.dpars.data.dirs.h5dir, 'LS-PARAMS.h5'))
+            if os.path.exists(os.path.join(self.io.folder, self.dpars.data.dirs.h5dir, 'RAW-STACK.h5')):
+                self.RAW_STACK=ts.loadh5(os.path.join(self.io.folder, self.dpars.data.dirs.h5dir, 'RAW-STACK.h5'))
+            if os.path.exists(os.path.join(self.io.folder, self.dpars.data.dirs.h5dir, 'PROC-STACK.h5')):
+                self.PROC_STACK=ts.loadh5(os.path.join(self.io.folder, self.dpars.data.dirs.h5dir, 'PROC-STACK.h5'))
+            if os.path.exists(os.path.join(self.io.folder, self.dpars.data.dirs.h5dir, 'LS-PARAMS.h5')):
+                self.LS_PARAMS=ts.loadh5(os.path.join(self.io.folder, self.dpars.data.dirs.h5dir, 'LS-PARAMS.h5'))
+            if os.path.exists(os.path.join(self.io.folder, self.dpars.data.dirs.h5dir, 'NSBAS-PARAMS.h5')):
+                self.NSBAS_PARAMS=ts.loadh5(os.path.join(self.io.folder, self.dpars.data.dirs.h5dir, 'NSBAS-PARAMS.h5'))
 
+    def ensemble_coherence(self, sol, weight=None):
+        '''ensemble_coherence(sol,weight=None)
+        Calculates the ensemble coherence based on complex interferograms (G.RAW-STACK) and solutions (G.LS-PARAMS). 
+        weight: 'long' or 'short' for btemp
+        '''
+        if not self.ppars.params.proc.demerr:
+            Jmat=self.RAW_STACK['Jmat']
+            recons=sol['recons']
+            subimage=self.dpars.data.subimage
+            dS=recons.shape #dataShape
+            recons=basic.tile(-recons[:,subimage.rxmin:subimage.rxmax, subimage.rymin:subimage.rymax].mean(axis=-1).mean(axis=-1), (dS[1]*dS[2],1)).T+recons.reshape([dS[0], dS[1]*dS[2]])
+            recons_interf=(basic.dot(Jmat, recons)/1000.)*(4.0*basic.pi/self.dpars.data.master.wavelength) #convert to meters and then radians.
+            data=self.RAW_STACK['igram']
+            dSi=data.shape[0]
+            data=basic.tile(-data[:,subimage.rxmin:subimage.rxmax, subimage.rymin:subimage.rymax].mean(axis=-1).mean(axis=-1), (dS[1]*dS[2],1)).T+data.reshape([dSi, dS[1]*dS[2]])
+            data_diff=data.reshape(recons_interf.shape)-recons_interf;
+            if weight is None:
+                ensemble_coherence=abs(basic.exp(1.j*(data_diff)).mean(axis=0))
+            elif weight=='long':
+                weight=abs(basic.dot(self.RAW_STACK['Jmat'], self.RAW_STACK['tims']))
+                weight=weight.reshape([1,dSi])
+                ensemble_coherence=abs(basic.dot(weight, basic.exp(1.j*(data_diff)))/weight.sum())
+            elif weight=='short':
+                weight=abs(basic.dot(self.RAW_STACK['Jmat'], self.RAW_STACK['tims']))
+                wmin=weight.min();wmax=weight.max()
+                weight=wmax-weight.reshape([1,dSi])+wmin #flip the weights
+                ensemble_coherence=abs(basic.dot(weight, basic.exp(1.j*(data_diff)))/weight.sum())
+            else:
+                print "weight can be :'long', 'short' or None."
+                return False
+            return ensemble_coherence.reshape([dS[1], dS[2]])
+        else:
+            print "Not implemented yet."
+            return False
+            
+        
