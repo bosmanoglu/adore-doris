trunk/gui/agooey.desktop   FEAT: Adding a Unity friendly desktop shortcut file trunk/lib/python/giant.py   BUGFIX: Correct ensemble coherence calculation. trunk/lib/python/basic/graphics/__init__.py   FEAT: Added new functions to clickScat trunk/scr/fun/report   BUGFIX:Report assumes square pixels now. trunk/scr/fun/i12s   FEAT: Added parameter (-i) to run command on a single interferogram.   BUGFIX: Improved the exit function for i12s plot. trunk/scr/fun/scenes   FEAT: Added rename bistatic for tsx trunk/scr/fun/crops   FEAT: Added parameter (-c) to run command on a single crop trunk/scr/fun/mask   BUGFIX: The default generate command was not working. trunk/scr/fun/functions   FEAT: Added visualize command trunk/scr/modifyRes.sh   BUGFIX: Corrected the help message.

trunk/lib/python/giant.py
@@ -47,34 +47,32 @@ class Object:
         Calculates the ensemble coherence based on complex interferograms (G.RAW-STACK) and solutions (G.LS-PARAMS). 
         weight: 'long' or 'short' for btemp
         '''
-        if not self.ppars.params.proc.demerr:
-            Jmat=self.RAW_STACK['Jmat']
-            recons=sol['recons']
-            subimage=self.dpars.data.subimage
-            dS=recons.shape #dataShape
-            recons=basic.tile(-recons[:,subimage.rxmin:subimage.rxmax, subimage.rymin:subimage.rymax].mean(axis=-1).mean(axis=-1), (dS[1]*dS[2],1)).T+recons.reshape([dS[0], dS[1]*dS[2]])
-            recons_interf=(basic.dot(Jmat, recons)/1000.)*(4.0*basic.pi/self.dpars.data.master.wavelength) #convert to meters and then radians.
-            data=self.RAW_STACK['igram']
-            dSi=data.shape[0]
-            data=basic.tile(-data[:,subimage.rxmin:subimage.rxmax, subimage.rymin:subimage.rymax].mean(axis=-1).mean(axis=-1), (dS[1]*dS[2],1)).T+data.reshape([dSi, dS[1]*dS[2]])
-            data_diff=data.reshape(recons_interf.shape)-recons_interf;
-            if weight is None:
-                ensemble_coherence=abs(basic.exp(1.j*(data_diff)).mean(axis=0))
-            elif weight=='long':
-                weight=abs(basic.dot(self.RAW_STACK['Jmat'], self.RAW_STACK['tims']))
-                weight=weight.reshape([1,dSi])
-                ensemble_coherence=abs(basic.dot(weight, basic.exp(1.j*(data_diff)))/weight.sum())
-            elif weight=='short':
-                weight=abs(basic.dot(self.RAW_STACK['Jmat'], self.RAW_STACK['tims']))
-                wmin=weight.min();wmax=weight.max()
-                weight=wmax-weight.reshape([1,dSi])+wmin #flip the weights
-                ensemble_coherence=abs(basic.dot(weight, basic.exp(1.j*(data_diff)))/weight.sum())
-            else:
-                print "weight can be :'long', 'short' or None."
-                return False
-            return ensemble_coherence.reshape([dS[1], dS[2]])
+        Jmat=self.RAW_STACK['Jmat']
+        recons=sol['recons']
+        subimage=self.dpars.data.subimage
+        dS=recons.shape #dataShape
+        #recons=basic.tile(-recons[:,subimage.rxmin:subimage.rxmax, subimage.rymin:subimage.rymax].mean(axis=-1).mean(axis=-1), (dS[1]*dS[2],1)).T+recons.reshape([dS[0], dS[1]*dS[2]])
+        recons=recons.reshape([dS[0], dS[1]*dS[2]])
+        recons_interf=(basic.dot(Jmat, recons)/1000.)*(4.0*basic.pi/self.dpars.data.master.wavelength) #convert to meters and then radians.
+        data=self.RAW_STACK['igram']
+        dSi=data.shape[0]
+        #data=basic.tile(-data[:,subimage.rxmin:subimage.rxmax, subimage.rymin:subimage.rymax].mean(axis=-1).mean(axis=-1), (dS[1]*dS[2],1)).T+data.reshape([dSi, dS[1]*dS[2]])
+        data_diff=basic.exp(1j*basic.wrapToPi(data.reshape(recons_interf.shape)-recons_interf));
+        if weight is None:
+            #ensemble_coherence=abs(basic.exp(1.j*(data_diff)).mean(axis=0))            
+            ensemble_coherence=abs(data_diff.sum(axis=0))/abs(data_diff).sum(axis=0)            
+        elif weight=='long':
+            weight=abs(basic.dot(self.RAW_STACK['Jmat'], self.RAW_STACK['tims']))
+            weight=weight.reshape([1,dSi])
+            ensemble_coherence=abs(basic.dot(weight, basic.exp(1.j*(data_diff)))/weight.sum())
+        elif weight=='short':
+            weight=abs(basic.dot(self.RAW_STACK['Jmat'], self.RAW_STACK['tims']))
+            wmin=weight.min();wmax=weight.max()
+            weight=wmax-weight.reshape([1,dSi])+wmin #flip the weights
+            ensemble_coherence=abs(basic.dot(weight, basic.exp(1.j*(data_diff)))/weight.sum())
         else:
-            print "Not implemented yet."
+            print "weight can be :'long', 'short' or None."
             return False
+        return ensemble_coherence.reshape([dS[1], dS[2]])
             
         