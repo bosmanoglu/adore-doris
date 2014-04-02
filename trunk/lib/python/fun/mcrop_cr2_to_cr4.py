7400f1653f61c7ae0a98c024035b2d3c0145d6f1
batuhan.osmanoglu <batuhan.osmanoglu@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2014-04-02 19:40:44 +0000

    lib/python/fun/mcrop_cr2_to_cr4.py BUGFIX: writing to cr4 as single file can cause out of memory. This divides in 10.

--- trunk/lib/python/fun/mcrop_cr2_to_cr4.py
+++ trunk/lib/python/fun/mcrop_cr2_to_cr4.py
@@ -1,4 +1,17 @@
+filesize=os.path.getsize(mobj.crop.Data_output_file);
+width=mobj.crop.Last_pixel-mobj.crop.First_pixel+1;
+datatype, complexFlag=adore.dataFormat2dataType('cr2');
+length=float(filesize)/width/np.dtype(datatype).itemsize/2; 
+fileparts=fix(linspace(0, length, 10));
+  
 data=adore.getdata(mobj.crop.Data_output_file,(mobj.crop.Last_pixel-mobj.crop.First_pixel+1), 'cr2', (mobj.crop.Last_line-mobj.crop.First_line+1));
-adore.writedata(mobj.crop.Data_output_file+'.cr4', data, 'cr4')
+#adore.writedata(mobj.crop.Data_output_file+'.cr4', data, 'cr4')
+fout=open(mobj.crop.Data_output_file+'.cr4', 'wb');
+for l in xrange(len(fileparts)-1):
+  bipData=np.empty([fileparts[l+1]-fileparts[l], data.shape[1]*2]);
+  bipData[:,0::2]=data[fileparts[l]:fileparts[l+1],:].real;
+  bipData[:,1::2]=data[fileparts[l]:fileparts[l+1],:].imag;
+  fout.write( bipData.astype(np.dtype(numpy.float32)) );
+fout.close();
 A.modifyResults('m_crop', 'Data_output_file', mobj.crop.Data_output_file+'.cr4');
 A.modifyResults('m_crop', 'Data_output_format', 'complex_real4')
