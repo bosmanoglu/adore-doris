4f37b32682b98035f456a866816f05c25517a6f9
batuhan.osmanoglu <batuhan.osmanoglu@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2014-04-02 20:02:02 +0000

    lib/python/fun/scrop_cr2_to_cr4.py, lib/python/fun/mcrop_cr2_to_cr4.py FEAT: Now it reads and writes 1/10th of the file at each time.

--- trunk/lib/python/fun/mcrop_cr2_to_cr4.py
+++ trunk/lib/python/fun/mcrop_cr2_to_cr4.py
@@ -2,15 +2,18 @@ filesize=os.path.getsize(mobj.crop.Data_output_file);
 width=mobj.crop.Last_pixel-mobj.crop.First_pixel+1;
 datatype, complexFlag=adore.dataFormat2dataType('cr2');
 length=float(filesize)/width/np.dtype(datatype).itemsize/2; 
-fileparts=fix(linspace(0, length, 10));
+fileparts=fix(linspace(0, length, 10)).astype(int);
   
-data=adore.getdata(mobj.crop.Data_output_file,(mobj.crop.Last_pixel-mobj.crop.First_pixel+1), 'cr2', (mobj.crop.Last_line-mobj.crop.First_line+1));
+#data=adore.getdata(mobj.crop.Data_output_file,(mobj.crop.Last_pixel-mobj.crop.First_pixel+1), 'cr2', (mobj.crop.Last_line-mobj.crop.First_line+1));
 #adore.writedata(mobj.crop.Data_output_file+'.cr4', data, 'cr4')
 fout=open(mobj.crop.Data_output_file+'.cr4', 'wb');
 for l in xrange(len(fileparts)-1):
+  data=adore.getdata(mobj.crop.Data_output_file,(mobj.crop.Last_pixel-mobj.crop.First_pixel+1), 'cr2', fileparts[l+1]-fileparts[l], skipbytes=(fileparts[l]*width*np.dtype(datatype).itemsize*2));
   bipData=np.empty([fileparts[l+1]-fileparts[l], data.shape[1]*2]);
-  bipData[:,0::2]=data[fileparts[l]:fileparts[l+1],:].real;
-  bipData[:,1::2]=data[fileparts[l]:fileparts[l+1],:].imag;
+  #bipData[:,0::2]=data[fileparts[l]:fileparts[l+1],:].real;
+  #bipData[:,1::2]=data[fileparts[l]:fileparts[l+1],:].imag;
+  bipData[:,0::2]=data.real;
+  bipData[:,1::2]=data.imag;
   fout.write( bipData.astype(np.dtype(numpy.float32)) );
 fout.close();
 A.modifyResults('m_crop', 'Data_output_file', mobj.crop.Data_output_file+'.cr4');
