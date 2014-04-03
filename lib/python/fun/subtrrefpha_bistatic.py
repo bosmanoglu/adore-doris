b234b022df2766a50dba962884eb89fae79e3064
batuhan.osmanoglu <batuhan.osmanoglu@d0d016b0-0a7a-11df-a26f-8b716316a5bc>
2014-04-03 20:09:12 +0000

    trunk/scr/process/subtrrefdem   Now uses floatmult, which is faster than using the python operation. trunk/lib/python/fun/subtrrefpha_bistatic.py   BUGFIX: Now gets the size right, if int_multilook is [1,1] but srp_multilook is > 1.

--- lib/python/fun/subtrrefpha_bistatic.py
+++ lib/python/fun/subtrrefpha_bistatic.py
@@ -1,6 +1,12 @@
+import insar
 cint=adore.getProduct(ires, 'interfero');
-x=linspace(-2,2,cint.shape[0])
-y=linspace(-2,2,cint.shape[1])
+srp_mutlilook=[0,0];
+srp_multilook[0]=int(setobj.subtrrefpha.srp_multilook.strip('"').split(" ")[0]);
+srp_multilook[1]=int(setobj.subtrrefpha.srp_multilook.strip('"').split(" ")[1]);
+if srp_multilook!=[1,1]:
+  cint=insar.multilook(cint, srp_multilook);
+x=linspace(-2,2,cint.shape[0]);
+y=linspace(-2,2,cint.shape[1]);
 Y,X=meshgrid(y,x);
 refpha=adore.polyval(X,Y, ires['comp_refphase']['Estimated_coefficients_flatearth']);
 srp=cint*exp(-1j*refpha/2);
