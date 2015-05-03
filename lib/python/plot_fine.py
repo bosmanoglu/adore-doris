f41187552ec14f52893f59be4a21bc9dd8e187d7
Batuhan Osmanoglu <batu@vpn-128-154-224-44.gsfc.nasa.gov>
2015-05-02 22:48:14 -0400

    lib/python/plot_coarsecorr.py, lib/python/plot_fine.py   FEAT: Added a scale vector using quiverkey. scr/fun/check   BUGFIX: Moving the checkUpdate setup to github.

--- lib/python/plot_fine.py
+++ lib/python/plot_fine.py
@@ -21,13 +21,22 @@ def main(argv=None):
 #figure1
   pylab.figure()
   pylab.title('Fine Correlation Results')
-  pylab.quiver(A[:,2], A[:,1], A[:,4], A[:,3], A[:,5]);
+  Q=pylab.quiver(A[:,2], A[:,1], A[:,4], A[:,3], A[:,5]);
+  azAvg=round(pylab.np.mean(A[:,3]));
+  rgAvg=round(pylab.np.mean(A[:,4]));
+  qk = pylab.quiverkey(Q, 0.33, 0.92, azAvg, ('%d [px az]' % (azAvg)), labelpos='W', fontproperties={'weight': 'bold'})
+  qk = pylab.quiverkey(Q, 0.66, 0.92, rgAvg, ('%d [px rg]' % (rgAvg)), labelpos='W', fontproperties={'weight': 'bold'})
   pylab.colorbar()
-#figure2
 
+#figure2
   pylab.figure()
   pylab.title('Fine Correlation Deviations')
   pylab.quiver(A[:,2], A[:,1], A[:,4]-rg, A[:,3]-az, A[:,5]);
+  Q=pylab.quiver(A[:,2], A[:,1], A[:,4]-rg, A[:,3]-az, A[:,5]);
+  azStd=round(pylab.np.std(A[:,3]-az));
+  rgStd=round(pylab.np.std(A[:,4]-rg));
+  qk = pylab.quiverkey(Q, 0.33, 0.92, azStd, ('%d [px az]' % (azStd)), labelpos='W', fontproperties={'weight': 'bold'})
+  qk = pylab.quiverkey(Q, 0.66, 0.92, rgStd, ('%d [px rg]' % (rgStd)), labelpos='W', fontproperties={'weight': 'bold'})
   pylab.colorbar()
   pylab.show()
 
